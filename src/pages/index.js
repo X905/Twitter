9c7e17bc2c28f5fdcb6c3559f9445909463a467b
Daniel Castillo <castillo.a@abposus.com>
2020-09-15 01:12:49 -0600

    grids - svg icons - color palette

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+
+export default function Home() {
+  return (
+    <div className="bg-body text-white text-primary grid grid-cols-3 mx-64 min-h-screen">
+      <nav>
+        <div>
+          Logo
+        </div>
+        <ul>
+          <li>
+            <span>icon</span>
+            <span>Home</span>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        tweets
+      </main>
+      <aside>
+        links
+      </aside>
+
+    </div>
+  )
+}
