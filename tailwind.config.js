ac7b79f62ee007139b4a75f7e95a4567c89deb65
Daniel Castillo <castillo.a@abposus.com>
2020-09-15 00:58:56 -0600

    adding tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        gray:{
+          900:'hsl(210, 27.5%, 28%)',
+        },
+        body:{
+          default: 'hsl(210, 34.4%, 12.5%)',
+        },
+        primary:{
+          default: 'hsl(202.8, 89.1%, 53.1%)',
+          600: 'hsl(202.8, 89.1%, 23.1%)',
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
